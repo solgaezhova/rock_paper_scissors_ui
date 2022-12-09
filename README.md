ed4874661338ecac8d4fa23d62465f98349bcabe
Olya Ezhova <olya.yezhova@gmail.com>
2022-12-08 20:09:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a well-known "Rock-paper-scissors" game written with JavaScript only. A game consists of 5 rounds, you can choose to type "rock", "paper", or "scissors" in a prompt window. You can see the user's choice and the computer's random choice in the Console. The score is being kept, and at the end you can see the winner in the Console. 
+
+You can test the game in the Console on the website here: https://solgaezhova.github.io/rock_paper_scissors/. Good luck!
+ 
---
+++ index.js
@@ -0,0 +1,61 @@
+const buttons = document.querySelectorAll("button");
+const playerScoreDisplay = document.querySelector(".player-score");
+const computerScoreDisplay = document.querySelector(".computer-score");
+const playerChoiceDisplay = document.querySelector(".player-choice");
+const computerChoiceDisplay = document.querySelector(".computer-choice");
+const roundResultDisplay = document.querySelector(".round-result-display");
+const winnerDisplay = document.querySelector(".winner-display");
+
+const options = ["rock", "paper", "scissors"]
+let computerPlay = () => {
+    return options[Math.floor(Math.random() * options.length)];
+}
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playerSelection = button.id;
+        console.log(playerSelection);
+        if (playerScore < 5 && computerScore < 5) {
+            playRound(playerSelection);
+        } else {
+            if (playerScore > computerScore) {
+                winnerDisplay.innerText = `You won this game with the score ${playerScore} to ${computerScore}. Congratulations!`;
+            }
+            else if (playerScore < computerScore) {
+                winnerDisplay.innerText = `You lost this game with the score ${playerScore} to ${computerScore}. Don't get upset, try again!`;
+            }
+            else {
+                winnerDisplay.innerText = "This game is a draw. Try again!";
+            }
+        }
+
+
+    });
+})
+
+const playRound = (playerSelection) => {
+
+    let computerSelection = computerPlay();
+
+    playerChoiceDisplay.innerText = playerSelection;
+    computerChoiceDisplay.innerText = computerSelection;
+
+    if (playerSelection == computerSelection) {
+        roundResultDisplay.innerText = "This round is a draw!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors"
+        || playerSelection == "paper" && computerSelection == "rock"
+        || playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore++;
+        playerScoreDisplay.innerText++;
+        roundResultDisplay.innerText = `You win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`
+    }
+    else {
+        computerScore++;
+        computerScoreDisplay.innerText++;
+        roundResultDisplay.innerText = `You lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`
+    }
+}
+
+const capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
